ca4f35fe85c98dc60ff6dd4c56d4093f53e079cd
Bradley-Coy <bradleycoy95@gmail.com>
2022-07-15 09:35:33 -0700

    added CodeWars

---
+++ Inheritance/God.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+///CODE WARS EXERCISE 2
+
+namespace Inheritance
+{
+
+    public class God
+    {
+        public static Human[] Create()
+        {
+            return new Human[] { new Man(), new Woman() };
+        }
+
+        public class Human
+        {
+        }
+
+        public class Man : Human
+        {
+        }
+        public class Woman : Human
+        {
+        }
+    }
+    
+}
+
